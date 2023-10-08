c058aface7d2a0f853d6cb2ef213ede0b13905e9
Nahimul Islam <nahimul.dti@gmail.com>
2023-10-09 01:38:24 +0600

    Initial commit

---
+++ Array.java
@@ -0,0 +1,14 @@
+public class Array {
+    public static void main (String[] args){
+
+        // Others can be initialized by passing values
+        // 
+        int [] array = {11,1,11,1,1,1,1,1,1,1,1,1,1,1,1,1};
+
+        for (int i=0; i<10 ; i++){
+
+            System.out.println("Array Item : "+array[i]);
+        }
+
+    }
+}
